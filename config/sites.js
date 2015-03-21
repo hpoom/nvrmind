f9efdab85b37169d94185e4ca84c10de2dea6f02
Simon Wood <simon.wood@holidayextras.com>
2015-03-21 10:00:41 +0000

    Looking at different ways to do the site config.

--- config/sites.js
+++ config/sites.js
@@ -1,13 +1,16 @@
-var sites = {}
+var sites = {};
 
 sites.test = {};
 sites.test.name = 'Test Site';
 sites.test.domain = 'http://test.com';
-sites.test.source = '?';
+sites.test.source = {};
+sites.test.destination = {};
 
-sites.hpoom = {};
-sites.hpoom.name = 'hpoom Blog';
-sites.hpoom.domain = 'http://hpoom.co.uk';
-sites.hpoom.source = '?';
+sites.hpoom = {
+	name: 'hpoom Blog',
+	domain: 'http://hpoom.co.uk',
+	source: {},
+	destination: {}
+};
 
 module.exports = sites;
